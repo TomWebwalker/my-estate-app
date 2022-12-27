1a38091e04082cf4ffdcb2fe62c03f9f055050a3
Tomasz Flis <tomaszwwl@gmail.com>
2022-12-27 10:52:53 +0100

    deploy

--- apps/my-estate-app/src/app/app.tsx
+++ apps/my-estate-app/src/app/app.tsx
@@ -1,11 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, Link, RouterProvider} from "react-router-dom";
 import {Auth} from "@my-estate-app/auth";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <div>Hello world! <Link to='auth'>Auth</Link></div>,
   },
   {
     path: '/auth',
