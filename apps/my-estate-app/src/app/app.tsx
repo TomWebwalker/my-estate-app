6a02a11efeaec3c42bad2095278acbf926056809
Tomasz Flis <tomaszwwl@gmail.com>
2022-12-27 10:33:35 +0100

    deploy

--- apps/my-estate-app/src/app/app.tsx
+++ apps/my-estate-app/src/app/app.tsx
@@ -1,13 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Auth} from "@my-estate-app/auth";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: '/auth',
+    element: <Auth/>
+  }
+]);
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="my-estate-app" />
-      <div />
-    </>
+    <RouterProvider router={router}/>
   );
 }
 
